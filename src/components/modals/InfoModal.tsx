6249f2969728d9c1fbfdf6b1e65c4842025ca166
Brayden Price <brayden@braydenprice.dev>
2022-04-27 21:56:16 -0400

    Update InfoModal so whitespace will display.

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -12,8 +12,11 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Guess the Biology related word in 6 tries. After each guess, the color
         of the tiles will change to show how close your guess was to the word.
-        <p className="underline font-bold color-red">HINT:</p> You don't have to
-        guess a Biology word, but the correct awnser will always be one.
+        <p className="underline font-bold color-red display-linebreak">
+          HINT:
+        </p>{' '}
+        You don't have to guess a Biology word, but the correct awnser will
+        always be one.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
